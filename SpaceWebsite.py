a29c8bded09325095b1a43d4bbfb5a90ca6e636e
Iamjonasg <46452933+Iamjonasg@users.noreply.github.com>
2022-05-15 01:18:42 -0400

    imported request module, started treating the data from the API

--- SpaceWebsite.py
+++ SpaceWebsite.py
@@ -1,2 +1,8 @@
-print("test")
-print("test2")
+import requests
+
+response = requests.get("https://lldev.thespacedevs.com/2.2.0/launch/?limit=99")
+print(response.status_code)
+raw = response.json()
+print(raw)
+results = raw["results"]
+print(len(results))
