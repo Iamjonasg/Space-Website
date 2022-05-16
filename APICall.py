fd781110cecd6cc2d07f575db7a1748a2e33bec7
Iamjonasg <46452933+Iamjonasg@users.noreply.github.com>
2022-05-16 19:08:00 -0400

    put all api calls into one launch list, formatted as a json object for later use, changed the file name

---
+++ APICall.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+response = requests.get("https://lldev.thespacedevs.com/2.2.0/launch/previous/?limit=100&offset=0")
+results = []
+print(response.status_code)
+raw = response.json()
+count = raw["count"]
+print(count)
+offset = 0
+while offset < count:
+    url = "https://lldev.thespacedevs.com/2.2.0/launch/previous/?limit=100&offset="+str(offset)
+    print(url)
+    response = requests.get(url)
+    raw = response.json()
+    results.extend(raw["results"])
+    offset += 100
+
+with open("launches.json", 'w') as file:
+    json.dump(results, file, indent=2)
+
--- SpaceWebsite.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-response = requests.get("https://lldev.thespacedevs.com/2.2.0/launch/?limit=99")
-print(response.status_code)
-raw = response.json()
-print(raw)
-results = raw["results"]
-print(len(results))
