52b4265e6947e1b0bde648359d1b889367c50f34
Iamjonasg <46452933+Iamjonasg@users.noreply.github.com>
2022-05-16 23:05:22 -0400

    added html and js basic website with chart.js, added year counter and success rate calculation

--- listfun.py
+++ listfun.py
@@ -7,10 +7,11 @@ successes = 0
 failures = 0
 others = 0
 count = 0
+years = {}
 for ele in launches:
     if ele['rocket']['configuration']['family'] == prompt:
         count += 1
-        print(ele['status']['abbrev'])
+
         if ele['status']['abbrev'] == 'Success':
             successes += 1
 
@@ -18,3 +19,18 @@ for ele in launches:
             failures += 1
         else:
             others += 1
+
+        window = str(ele['window_end'])
+        year = window.partition("-")[0]
+        if year in years:
+            years[year] += 1
+        else:
+            years[year] = 1
+
+print(years)
+print(successes)
+print(failures)
+print(others)
+
+
+print(successes/(successes+failures)*100)
---
+++ space-website.js
@@ -0,0 +1,39 @@
+
+var pieExists = false;
+
+
+$(document).ready(function() {
+	createPie()
+
+	$("#lancer").click(function(){
+		createPie();
+	});
+});
+
+
+function createPie(){
+	
+	if (pieExists) {
+		piechart.destroy();
+		
+	}
+
+
+
+	piechart = new Chart($('#pie-chart'), {
+		type: 'pie',
+		data: {
+			labels: ["Success", "Failure", "Other"],
+			datasets: [{
+				data: [155, 5, 3],
+				backgroundColor:['green','red','grey']	
+			  }]
+
+		}
+	  });
+	pieExists = true;
+	
+	
+}
+
+
