b63151f5f8f9f94c0f1d087344ffb20eb8f0ba81
Iamjonasg <46452933+Iamjonasg@users.noreply.github.com>
2022-05-20 00:14:08 -0400

    added a lot

--- generatenamelists.py
+++ generatenamelists.py
@@ -4,15 +4,22 @@ file = open("launches.json")
 launches = json.load(file)
 rocketnames = []
 rocketfamilies = []
-
+rocketfullnames = []
+agencies = []
 i = 0
 while i < len(launches):
     currentname = launches[i]['rocket']['configuration']['name']
     currentfamily = launches[i]['rocket']['configuration']['family']
+    currentfullname = launches[i]['rocket']['configuration']['full_name']
+    currentagency = launches[i]['launch_service_provider']['name']
     if currentname not in rocketnames:
         rocketnames.append(currentname)
     if currentfamily not in rocketfamilies:
         rocketfamilies.append(currentfamily)
+    if currentfullname not in rocketfullnames:
+        rocketfullnames.append(currentfullname)
+    if currentagency not in agencies:
+        agencies.append(currentagency)
     i+=1
 
 
@@ -21,3 +28,9 @@ with open("names.json", 'w') as file:
 
 with open("families.json", 'w') as file:
     json.dump(rocketfamilies, file, indent=1)
+
+with open("fullnames.json", 'w') as file:
+    json.dump(rocketfullnames, file, indent=1)
+
+with open("agencies.json", 'w') as file:
+    json.dump(agencies, file, indent=1)
